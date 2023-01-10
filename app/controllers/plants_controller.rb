3b164e4a660fab1b01b1c966a365f7ce726c0eac
Patrick Young <patrickyoung281@gmail.com>
2023-01-10 15:26:53 -0500

    commit tree 0d5135c8d08d7d99e0839032baac4f9a927be1ac

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,27 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant = Plant.find_by(id: params[:id])
+    if plant 
+      plant.update(is_in_stock: false)
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+
+def destroy
+  plant = Plant.find_by(id: params[:id])
+  if plant
+    plant.destroy
+    head :no_content
+  else
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+end
+
+  end
+
   private
 
   def plant_params
